8d3d17354cadf934496bdac0bc1273990d49df92
BruninAlv61 <alvarezbruno.dev@gmail.com>
2026-02-13 23:35:47 -0300

    feat(search-form): Deberes de la clase  - Completar el resto de filtros (Ubicacion, Nivel de experiencia)  - Usar onFocus y onBlur  - Unificar el sistema de busqueda eligiendo el onChange o el onSubmit
     Decisiones:
     - Decidi usar el onFocus y onBlur con un state de React para darle un borde al input
       principal cuando esté activo, a decir verdad no creo que sea el mejor caso de uso
       ya que eso tranquilamente se puede hacer con CSS puro en vez de con React/JS y
       ademas de ser mas sencillo, consume muchos menos recursos
     - En cuanto a las busquedas por filtros, decidí usar la busqueda en tiempo real con
       onChange para una mejor UX, para hacer esto tuve que unificar el input de texto
       junto a los demas filtros y en el FormData, pasarle como el parametro el event.target.form

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -13,28 +13,26 @@ function App() {
   const { jobs, loading, error } = useJobs();
 
   const [filters, setFilters] = useState({
+    search: '',
     technology: '',
     location: '',
     experienceLevel: ''
   })
-  const [textToFilter, setTextToFilter] = useState('')
   const [currentPage, setCurrentPage] = useState(1)
 
   const jobsFilteredByFilters = jobs.filter(job => {
     return (
-      (filters.technology === '' || job.data.technology.includes(filters.technology.toLowerCase()))
+      (filters.search === '' || job.titulo.toLowerCase().trim().includes(filters.search.toLowerCase().trim())) &&
+      (filters.technology === '' || job.data.technology.includes(filters.technology.toLowerCase())) &&
+      (filters.location === '' || job.data.modalidad === filters.location) &&
+      (filters.experienceLevel === '' || job.data.nivel === filters.experienceLevel)
+      
     )
   })
-  
-  const jobsWithTextToFilter = textToFilter === ''
-  ? jobsFilteredByFilters
-  : jobsFilteredByFilters.filter(job => {
-    return job.titulo.toLowerCase().trim().includes(textToFilter.toLowerCase().trim())
-  })
 
-  const totalPages = Math.ceil(jobsWithTextToFilter.length / RESULTS_PER_PAGE)
+  const totalPages = Math.ceil(jobsFilteredByFilters.length / RESULTS_PER_PAGE)
   
-  const pagedResults = jobsWithTextToFilter.slice(
+  const pagedResults = jobsFilteredByFilters.slice(
     (currentPage - 1) * RESULTS_PER_PAGE, //Página 1 -> 0, Página 2 -> 5, Página 3 -> 10
     currentPage * RESULTS_PER_PAGE        // Página 1 -> 5, Página 2 -> 10, Página 3 -> 15
   )
@@ -50,17 +48,13 @@ function App() {
     setCurrentPage(1)
   }
 
-  const handleTextFilter = (newTextToFilter) => {
-    setTextToFilter(newTextToFilter)
-    setCurrentPage(1)
-  }
 
   return (
     <>
       <Header />
 
       <main>
-        <SearchFormSection onSearch={handleSearch} onTextFilter={handleTextFilter}/>
+        <SearchFormSection onSearch={handleSearch}/>
 
         <section>
           <JobListings jobs={pagedResults} loading={loading} error={error}/>
--- react/src/components/SearchFormSection.jsx
+++ react/src/components/SearchFormSection.jsx
@@ -1,17 +1,19 @@
-import { useId } from "react"
+import { useId, useState } from "react"
 
-export function SearchFormSection({ onSearch, onTextFilter}) {
+export function SearchFormSection({ onSearch}) {
+  const [focusedField, setFocusedField] = useState(null)
+
   const idText = useId()
   const idTechnology = useId()
   const idLocation = useId()
   const idExperienceLevel = useId()
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
+  const handleChange = (event) => {
 
-    const formData = new FormData(event.target)
+    const formData = new FormData(event.target.form)
 
     const filters = {
+      search: formData.get(idText),
       technology: formData.get(idTechnology),
       location: formData.get(idLocation),
       experienceLevel: formData.get(idExperienceLevel)
@@ -20,18 +22,16 @@ export function SearchFormSection({ onSearch, onTextFilter}) {
     onSearch(filters)
   }
 
-  const handleTextChange = (event) => {
-    const text = event.target.value
-    onTextFilter(text)
-  }
+  const inputStyleFocus = focusedField ? 'solid 5px #0d3f6e' : 'none'
+
 
   return (
     <section className="jobs-search">
       <h1>Encuentra tu próximo trabajo</h1>
       <p>Explora miles de oportunidades en el sector tecnológico.</p>
 
-      <form onSubmit={handleSubmit} id="empleos-search-form" role="search">
-        <div className="search-bar">
+      <form id="empleos-search-form" role="search">
+        <div className="search-bar" style={{border: inputStyleFocus}}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -53,13 +53,15 @@ export function SearchFormSection({ onSearch, onTextFilter}) {
             id="empleos-search-input"
             type="text"
             placeholder="Buscar trabajos, empresas o habilidades"
-            onChange={handleTextChange}
+            onChange={handleChange}
+            onFocus={() => { setFocusedField(true)}}
+            onBlur={() => { setFocusedField(false)}}
           />
-          <button type="submit" style={{ position: 'absolute', right: 0, marginRight: 5}}>Buscar</button>
+          {/*<button type="submit" style={{ position: 'absolute', right: 0, marginRight: 5}}>Buscar</button>*/}
         </div>
 
         <div className="search-filters">
-          <select name={idTechnology} id="filter-technology">
+          <select name={idTechnology} id="filter-technology" onChange={handleChange}>
             <option value="">Tecnología</option>
             <optgroup label="Tecnologías populares">
               <option value="javascript">JavaScript</option>
@@ -77,7 +79,7 @@ export function SearchFormSection({ onSearch, onTextFilter}) {
             <option value="php">PHP</option>
           </select>
 
-          <select name={idLocation} id="filter-location">
+          <select name={idLocation} id="filter-location" onChange={handleChange}>
             <option value="">Ubicación</option>
             <option value="remoto">Remoto</option>
             <option value="cdmx">Ciudad de México</option>
@@ -86,7 +88,7 @@ export function SearchFormSection({ onSearch, onTextFilter}) {
             <option value="barcelona">Barcelona</option>
           </select>
 
-          <select name={idExperienceLevel} id="filter-experience-level">
+          <select name={idExperienceLevel} id="filter-experience-level" onChange={handleChange}>
             <option value="">Nivel de experiencia</option>
             <option value="junior">Junior</option>
             <option value="mid">Mid-level</option>
